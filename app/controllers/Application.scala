9dc6c7c8459158e66ed68a9e6b6b438fb41c06e1
shunsuke <shunsuke10suzuki22@gmail.com>
2013-03-05 07:12:13 +0900

    firstcommit

---
+++ app/controllers/Application.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import com.sun.xml.internal.ws.resources.WsdlmodelMessages
+
+object Application extends Controller {
+  
+  def index = Action {
+
+    System.out.println("branch a!!!!!");
+
+    Ok(views.html.index("Your new application is ready."))
+  }
+}
